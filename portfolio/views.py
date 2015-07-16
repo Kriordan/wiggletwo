2ad7f2a359ffaeb43b5d1de65d9eb39642083f8d
kriordan <keith.riordan@gmail.com>
2015-07-16 17:55:46 -0400

    Add pyyaml and yamlfiles for home and resume. load linedrawings to home

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,10 @@
+import yaml
+import os
+
+from django.conf import settings
 from django.shortcuts import render
 
+LINEDRAWINGS = yaml.load( open( 'yamlfiles/portfolio/linedrawings.yml' ) )
+
 def home(request):
-    return render(request, 'portfolio/home.html')
+    return render(request, 'portfolio/home.html', { 'linedrawings' : LINEDRAWINGS } )
