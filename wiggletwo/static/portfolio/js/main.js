08f2944c2bc84ade636ef5e9ada990a6e1eb3b32
kriordan <keith.riordan@gmail.com>
2015-07-21 22:45:34 -0400

    Add hosted jquery file. More styling of home template.

--- wiggletwo/static/portfolio/js/main.js
+++ wiggletwo/static/portfolio/js/main.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
     $('.linedrawing-carousel').slick({
         autoplay: true,
         dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 });
