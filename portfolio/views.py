28eef3c60bd9ce632e5e719188f3e6ce9a3c6bfd
kriordan <keith.riordan@gmail.com>
2015-09-14 23:51:34 -0400

    Modify resume template to use resume yml data. Style resume popover copy.

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.shortcuts import render
 
 LINEDRAWINGS = yaml.load( open( 'yamlfiles/portfolio/linedrawings.yml' ) )
+RESUME = yaml.load( open( 'yamlfiles/portfolio/resume.yml' ) )
 
 def home(request):
-    return render(request, 'portfolio/home.html', { 'linedrawings' : LINEDRAWINGS } )
+    return render(request, 'portfolio/home.html', { 'linedrawings' : LINEDRAWINGS, 'resume' : RESUME } )
