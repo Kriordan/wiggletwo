6178600c0df9b07be318923e0d9cd0d8f04385d5
kriordan <keith.riordan@gmail.com>
2015-10-05 20:46:11 -0400

    Update styles for the home page Add social icons for footer Change logo

--- wiggletwo/static/portfolio/js/main.js
+++ wiggletwo/static/portfolio/js/main.js
@@ -2,16 +2,8 @@ $(document).ready(function(){
 
     $('.linedrawing-carousel').slick({
         autoplay: true,
-        dots: true,
-        slidesToShow: 3,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
+        dots: false,
+        slidesToShow: 1
     });
 
     $('.home-resume-popover').fancybox();
