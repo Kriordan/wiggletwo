3b56f30ae829a6d1c853bba129eec79eb6136dd9
kriordan <keith.riordan@gmail.com>
2015-10-05 21:37:41 -0400

    Add scroll arrow and scroll animation to home

--- wiggletwo/static/portfolio/js/main.js
+++ wiggletwo/static/portfolio/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    // Homepage
+
     $('.linedrawing-carousel').slick({
         autoplay: true,
         dots: false,
@@ -7,5 +9,22 @@ $(document).ready(function(){
     });
 
     $('.home-resume-popover').fancybox();
+
+    $('[rel=home-scrollarrow]').click( function() {
+        $('html, body').animate({
+            scrollTop: $('.home-secondbanner').offset().top
+        }, 500);
+        return false;
+    })
+
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+
+        if(height  > 400) {
+            $('[rel=home-scrollarrow]').addClass('hidearrow');
+        } else {
+            $('[rel=home-scrollarrow]').removeClass('hidearrow');
+        }
+    });
 
 });
